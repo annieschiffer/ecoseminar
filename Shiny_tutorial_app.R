c8dba4a6322e0eaf98a36e079ae5cf8254746910
annieschiffer <annie.schiffer@emory.edu>
2021-04-09 10:21:34 -0700

    Created interface and server

--- Shiny_tutorial_app.R
+++ Shiny_tutorial_app.R
@@ -1,1 +1,27 @@
 #R Shiny Tutorial
+
+#Attach necessary packages
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+# Attach necessary packages
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+# Create the user interface (create what the user sees and interacts with):
+ui <- fluidPage()
+
+# Create the server function (builds outputs based on what the user inputs):
+server <- function(input, output) {}
+
+# Combine them into an app:
+shinyApp(ui = ui, server = server)
+
+#Create a widget (collect information from the user that will produce outputs):
+
+#dropdown selection
+#checklist
+#slider bar
+
